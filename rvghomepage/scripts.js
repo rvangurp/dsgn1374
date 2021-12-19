bb424b6fec31075255e56db5fba05e50e2354154
rvangurp <71617393+rvangurp@users.noreply.github.com>
2021-12-18 20:31:58 -0500

    v1

---
+++ rvghomepage/scripts.js
@@ -0,0 +1,14 @@
+/* PERSONAL HOMEPAGE */
+
+console.log('Welcome to my personal homepage!')
+
+/* OFFSCREEN MENU */
+
+function nav(tgl = 'toggle', cls = 'nav-active') {
+	if (document.querySelector(`.${tgl}`)) {
+		document.querySelector(`.${tgl}`).addEventListener('click',
+			() => { document.body.classList.toggle(cls) }
+		)
+	}
+}
+nav()
